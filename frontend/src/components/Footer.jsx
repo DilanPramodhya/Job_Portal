541bba0d065ccdbea4cb7197b721f35d457543d4
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-11-22 23:48:58 +0530

    Footer, Navbar done in Frontend

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,85 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  FaLinkedin,
+  FaSquareInstagram,
+  FaSquareTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
 const Footer = () => {
-  return <div>Footer</div>;
+  const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <footer>
+        <div>
+          <img src="/vite.svg" alt="Logo" />
+        </div>
+        <div>
+          <h4>Support</h4>
+          <ul>
+            <li>No. 06, Keenakele Group, Mudukatuwa, Marawila</li>
+            <li>smttravelsandtours3118@gmail.com</li>
+            <li>+94 70 654 3001, +94 77 375 9938</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/jobs"}>Jobs</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+
+          <ul>
+            <li>
+              <Link to={"/linkdin"}>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/youtube"}>
+                <span>
+                  <FaYoutube />
+                </span>
+                <span>You Tube</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/instagram"}>
+                <span>
+                  <FaSquareInstagram />
+                </span>
+                <span>Instagram</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/twitter"}>
+                <span>
+                  <FaSquareTwitter />
+                </span>
+                <span>Twitter (X)</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">&copy; CopyRight2024. All Right Reserved</div>
+    </>
+  );
 };
 
 export default Footer;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [show, setShow] = useState();
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  return (
+    <>
+      <nav className={show ? "navbar show_navbar" : "navbar"}>
+        <div className="logo">
+          <img src="/vite.svg" alt="Logo" />
+          <h4>SMT</h4>
+        </div>
+        <div className="links">
+          <ul>
+            <li>
+              <Link to={"/"} onClick={() => setShow(!show)}>
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link to={"/jobs"} onClick={() => setShow(!show)}>
+                JOBS
+              </Link>
+            </li>
+            {isAuthenticated ? (
+              <li>
+                <Link to={"/dashboard"} onClick={() => setShow(!show)}>
+                  DASHBOARD
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link to={"/login"} onClick={() => setShow(!show)}>
+                  LOGIN
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <GiHamburgerMenu className="hamburger" onClick={()=> setShow(!show)} />
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
